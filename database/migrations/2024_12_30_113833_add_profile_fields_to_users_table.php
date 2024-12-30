6c81f8dc0edf455c9136e59a575e6132a4fa4c9b
Nedyo-hub <ilyas.fariss@student.ehb.be>
2024-12-30 12:42:47 +0100

    migration files geupdate voor profielpagina

--- database/migrations/2024_12_30_113833_add_profile_fields_to_users_table.php
+++ database/migrations/2024_12_30_113833_add_profile_fields_to_users_table.php
@@ -6,19 +6,26 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('role')->default('user'); //Nieuwe user
+            $table->string('username')->nullable();
+            $table->date('birthday')->nullable();
+            $table->string('profile_picture')->nullable();
+            $table->text('about_me')->nullable();
         });
     }
 
-    
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('role'); 
+            $table->dropColumn(['username', 'birthday', 'profile_picture', 'about_me']);
         });
     }
 };
